21cdc21d7953054349725e63b6d47faf31a4ca22
w24007 <w24007@osfl.ac.jp>
2025-05-27 11:27:38 +0900

    chapter1

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ my-app/app/page.tsx
@@ -0,0 +1,39 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+import { getNewsList } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
+import NewsList from "./_components/NewsList";
+import ButtonLink from "@/app/_components/ButtonLink";
+
+export default async function Home() {
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
+
+  return (
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルガンパ二ーです。
+          </p>
+        </div>
+        <Image
+          className={styles.bgimg}
+          src="/img-mv.jpg"
+          alt=""
+          width={4000}
+          height={1200}
+        />
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={data.contents} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/new">もっとみる</ButtonLink>
+        </div>
+      </section>
+    </>
+  );
+}
